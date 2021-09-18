754c801ceff89cc84886953e2705f403e91028bf
Stenio Oliveira da Cruz <stenio22@gmail.com>
2021-09-18 09:49:36 -0300

    correcao codigo

--- src/terceiroExercicio/AnnalynsInfiltration.java
+++ src/terceiroExercicio/AnnalynsInfiltration.java
@@ -1,5 +1,20 @@
 package terceiroExercicio;
 
+/*
+ *Instruções
+Neste exercício, você implementará a lógica de busca para um novo jogo de RPG que um amigo está desenvolvendo. O personagem principal do jogo é Annalyn, uma garota corajosa com um cão de estimação feroz e leal. Infelizmente, ocorre um desastre, já que sua melhor amiga foi sequestrada enquanto procurava frutas na floresta. Annalyn tentará encontrar e libertar seu melhor amigo, opcionalmente levando seu cachorro com ela nesta missão.
+
+Depois de algum tempo seguindo a trilha de sua melhor amiga, ela encontra o acampamento em que sua melhor amiga está presa. Acontece que existem dois sequestradores: um poderoso cavaleiro e um arqueiro astuto.
+
+Tendo encontrado os sequestradores, Annalyn considera em qual das seguintes ações ela pode se envolver:
+
+Ataque rápido: um ataque rápido pode ser feito se o cavaleiro estiver dormindo, pois leva tempo para ele colocar sua armadura, então ele estará vulnerável.
+Espião: o grupo pode ser espiado se pelo menos um deles estiver acordado. Caso contrário, espionar é uma perda de tempo.
+Sinalizar prisioneiro: o prisioneiro pode ser sinalizado por sons de pássaros se o prisioneiro estiver acordado e o arqueiro estiver dormindo, pois os arqueiros são treinados em sinalização de pássaros, para que possam interceptar a mensagem.
+Prisioneiro livre: se o prisioneiro estiver acordado e os outros dois personagens estiverem dormindo, uma entrada sorrateira no campo pode libertar o prisioneiro. Isso não funcionará se a prisioneira estiver dormindo, pois a prisioneira ficará assustada com a aparição repentina de sua amiga e o cavaleiro e arqueiro serão acordados. O prisioneiro também pode ser libertado se o arqueiro estiver dormindo e Annalyn estiver com seu cachorro de estimação, pois o cavaleiro ficará assustado com o cachorro e se retirará, e o arqueiro não pode equipar seu arco rápido o suficiente para evitar que o prisioneiro seja libertado.
+Você tem quatro tarefas: implementar a lógica para determinar se as ações acima estão disponíveis com base no estado dos três personagens encontrados na floresta e se o cão de estimação de Annalyn está presente ou não.
+ */
+
 class AnnalynsInfiltration {
 
 	public static boolean canFastAttack(boolean knightIsAwake) {
@@ -27,20 +42,31 @@ class AnnalynsInfiltration {
 		}
 		return false;
 	}
-
+	
 	public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
 			boolean petDogIsPresent) {
+			return (prisonerIsAwake && !archerIsAwake && !knightIsAwake) || (!archerIsAwake && petDogIsPresent);}
 
-		if ((knightIsAwake && archerIsAwake == false) && (prisonerIsAwake == true)) {
+	/* POR ALGUM MOTIVO  DOIS ERROS OCORRERAM NAS CONDIÇÕES DESTE COMENTARIO.
+	 
+	 
+	 	FAILED
+		Teste 2
+		Não pode libertar o prisioneiro se apenas o arqueiro estiver dormindo e o cão de estimação 		estiver ausente
+
+		FAILED
+		Teste 20
+		pode libertar o prisioneiro se apenas o prisioneiro estiver acordado e o cão de estimação 		estiver ausente
+	 * 
+	
+		if (((knightIsAwake && archerIsAwake == false) && (prisonerIsAwake == true)) || ( archerIsAwake == false && petDogIsPresent == true)) {
 			return true;
 		}
-		if (archerIsAwake == false && petDogIsPresent == true) {
-			return true;
-		}else {
+		else {
 			return false;
 		}
-	}
-
+		
+*/
 	public static void main(String[] args) {
 		System.out.println(AnnalynsInfiltration.canFastAttack(true));
 		System.out.println(AnnalynsInfiltration.canSpy(false, true, false));
@@ -50,3 +76,22 @@ class AnnalynsInfiltration {
 	}
 
 }
+
+	/*
+	 *  CODIGO SIMPLIFICADO ** ESTUDAR 
+	 class AnnalynsInfiltration {
+	    public static boolean canFastAttack(boolean knightIsAwake) {
+	    	return !knightIsAwake;
+	    }
+	    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean 			prisonerIsAwake) {
+	    	return (knightIsAwake || archerIsAwake || prisonerIsAwake);
+	    }
+	    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+	    	return !archerIsAwake && prisonerIsAwake;
+	    }
+		public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean 			prisonerIsAwake,
+				boolean petDogIsPresent) {
+				return (prisonerIsAwake && !archerIsAwake && !knightIsAwake) || (!archerIsAwake && 			petDogIsPresent);
+		}
+	}
+*/
